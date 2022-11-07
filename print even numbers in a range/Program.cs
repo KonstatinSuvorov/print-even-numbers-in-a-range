3006d2febea8dcd64d04afbdaac85adc1646c5df
Константин Суворов <kniaze84@outlook.com>
2022-11-07 22:51:37 +0300

    Добавьте файлы проекта.

---
+++ print even numbers in a range/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+/*
+Пользователь вводит с клавиатуры два числа. Нужно
+показать все четные числа в указанном диапазоне. Если
+границы диапазона указаны неправильно требуется про-
+извести нормализацию границ. Например, пользователь
+ввел 20 и 11, требуется нормализация, после которой
+начало диапазона станет равно 11, а конец 20.
+*/
+
+namespace print_even_numbers_in_a_range
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int c = 0;
+            Console.WriteLine("Введите два числа:\t");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b > a)
+            {
+                for(int i = a; i < b; i++)
+                {
+                    if (i % 2 == 0)Console.WriteLine(i);
+                }
+            }
+            if (a > b)
+            {
+                for (int i = b; i < a; i++)
+                {
+                    if (i % 2 == 0)Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
